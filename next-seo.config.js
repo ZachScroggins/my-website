a61e4e71500005eae0453d6795b1c2002594ab29
ZachScroggins <58402189+ZachScroggins@users.noreply.github.com>
2021-02-27 20:15:57 -0600

    og image

--- next-seo.config.js
+++ next-seo.config.js
@@ -9,6 +9,12 @@ export default {
       'I love building websites and web apps with cutting edge technologies. I specialize in JavaScript and have the most experience working with React and Node. I really enjoy the JAMstack, but I am also familiar with the LAMP stack, Python, Java, C#, and C++.',
     images: [
       {
+        url: 'https://zachscroggins.com/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Logo',
+      },
+      {
         url: 'https://zachscroggins.com/android-chrome-512x512.png',
         width: 512,
         height: 512,
